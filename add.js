da7d1d12f14fffdc07a665edeb12667ea8d9ffe9
Ivan <dmykh1993@gmail.com>
2022-03-09 21:17:26 +0300

    My first program

---
+++ add.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: true,
+};
+const a = prompt("Один из последних просмотренных фильмоы?", ""),
+  b = +prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмоы?", ""),
+  d = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
